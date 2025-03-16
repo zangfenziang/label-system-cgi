c7bbb3bed98a219ea5e298fd18cddf2898e2bd71
fxxzhang <fxxzhang@tencent.com>
2025-03-16 09:35:51 +0800

    feat: 调整接口

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //配置cors
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
