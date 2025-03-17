62b16aab7009fa2e3d6f7370323d02273f6b5c84
fxxzhang <fxxzhang@tencent.com>
2025-03-17 22:21:39 +0800

    feat: 增加pm2支持

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import { All, Controller, StreamableFile } from '@nestjs/common';
 import { createReadStream } from 'fs';
+import { Public } from './user/auth.guard';
 
 @Controller()
 export class MainController {
+  @Public()
   @All('/*')
   redirect() {
     return new StreamableFile(
