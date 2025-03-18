8e09e0a4d38aea0a33e0c273d8cb122dc3e37347
fxxzhang <fxxzhang@tencent.com>
2025-03-18 21:43:14 +0800

    feat: 调整bodylimit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   //配置cors
   app.enableCors();
+  app.useBodyParser('json', { limit: '10mb' });
 
   await app.listen(3000);
 }
